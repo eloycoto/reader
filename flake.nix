084eae43c1018c2bacb50f614853ea9db9f5bd4e
Eloy Coto <eloy.coto@acalustra.com>
2025-04-13 10:40:01 +0200

    Change flake for package

--- flake.nix
+++ flake.nix
@@ -1,27 +1,56 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-    flake-utils.url = "github:numtide/flake-utils";
     rust-overlay.url = "github:oxalica/rust-overlay";
   };
-  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
+
+  outputs = { self, nixpkgs, rust-overlay }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
+    in
+    {
+      packages = forAllSystems (system:
         let
-          overlays = [ (import rust-overlay) ];
           pkgs = import nixpkgs {
-            inherit system overlays;
+            inherit system;
+            overlays = [ (import rust-overlay) ];
+          };
+          reader = pkgs.rustPlatform.buildRustPackage {
+            pname = "reader";
+            version = "0.1.0";
+            src = ./.;
+            cargoLock = {
+              lockFile = ./Cargo.lock;
+            };
+            nativeBuildInputs = with pkgs; [
+              pkg-config
+            ];
+            buildInputs = with pkgs; [
+              openssl
+            ];
           };
         in
-        with pkgs;
         {
-          devShells.default = mkShell {
+          default = reader;
+          reader = reader;
+        }
+      );
+
+      devShells = forAllSystems (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ (import rust-overlay) ];
+          };
+        in
+        {
+          default = pkgs.mkShell {
             buildInputs = with pkgs; [
               (rust-bin.stable.latest.default.override {
                 extensions = [ "rust-src" ];
                 targets = [ "thumbv6m-none-eabi" ];
               })
-            ] ++  [
               rust-analyzer
               rustfmt
               openssl
@@ -30,4 +59,5 @@
           };
         }
       );
+    };
 }
